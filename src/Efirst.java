4d8c1e9c95e3e498adaa3f1b23a5660d1c3fa71f
neer2808 <uic.neerajkhanna@gmail.com>
2020-07-19 11:19:23 +0530

    // init commit

---
+++ src/Efirst.java
@@ -0,0 +1,24 @@
+
+
+public class Efirst {
+ int rollno;
+ String name;
+
+  }
+
+  class test
+{
+  public static void main(String[] args) {
+    Efirst obj= new Efirst();
+    obj.name= "rr";
+    System.out.println(obj.name);
+  }
+        }
+
+
+
+
+
+
+
+
